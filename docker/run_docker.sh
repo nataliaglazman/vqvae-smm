cccb3a9d820e5420f6f81bb25d31d3ad33b81301
Natalia Glazman <glazmann@yahoo.co.uk>
2026-03-10 22:13:02 +0000

    change docker files

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,4 +1,4 @@
-runai submit --name multiview-crl-change-content-size \
+runai submit --name vqvae-smm \
  -i aicregistry:5000/nglazman:multiview-crl-vqvae-latest \
  --node-type A100 \
  --run-as-user \
@@ -6,4 +6,4 @@ runai submit --name multiview-crl-change-content-size \
  --cpu 16 \
  --cpu-limit 32 \
  --memory 64G --memory-limit 128G --project nglazman \
- -v /nfs:/nfs --large-shm --command -- bash /nfs/home/nglazman/crl-2/multiview-crl/docker/run_training.sh
+ -v /nfs:/nfs --large-shm --command -- bash /nfs/home/nglazman/vqvae-smm/docker/run_training.sh
--- docker/run_training.sh
+++ docker/run_training.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PYTHONPATH=/nfs/home/nglazman/crl-2/multiview-crl \
 python /nfs/home/nglazman/vqvae-smm/train.py 
