0543f0d28dc5005313cfabbe15fdf3bb206f1c65
Natalia Glazman <glazmann@yahoo.co.uk>
2026-03-11 14:48:17 +0000

    update

--- docker/run_eval.sh
+++ docker/run_eval.sh
@@ -2,5 +2,6 @@
 set -euo pipefail
 
 python /nfs/home/nglazman/vqvae-smm/eval.py --mode rf-both \
+    --dataroot /nfs/home/nglazman/ADNI_registered \
     --checkpoint /nfs/home/nglazman/results/vqvae/checkpoint_best.pt \
     --save rf_combined.png
