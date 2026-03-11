c189b1ff4e669870c5d4eb1fedc2094bcda79136
Natalia Glazman <glazmann@yahoo.co.uk>
2026-03-11 15:17:17 +0000

    update docker files

--- docker/run_training.sh
+++ docker/run_training.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python /nfs/home/nglazman/vqvae-smm/train.py 
+python /nfs/home/nglazman/vqvae-smm/train.py --model-id vqvae-feature-maps \
+    --dataroot /nfs/home/nglazman/ADNI_registered \
+    --csv-path /nfs/home/nglazman/cluster/labels_cleaned_3class.csv \
+    --batch-size 4 \
+    --lr 1e-4 \
+    --train-steps 30000 \
