8c88170b29a598715b7a6806409baf7b3ce684e7
Natalia Glazman <glazmann@yahoo.co.uk>
2026-03-11 14:31:05 +0000

    update requirements

--- docker/run_eval.sh
+++ docker/run_eval.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python /nfs/home/nglazman/vqvae-smm/train.py --evaluate --checkpoint /nfs/home/nglazman/results/vqvae/checkpoint_best.pt \
-    --dataroot /nfs/home/nglazman/ADNI_registered \
-    --csv-path /nfs/home/nglazman/cluster/labels_cleaned_3class.csv
+python /nfs/home/nglazman/vqvae-smm/eval.py --mode rf-both \
+    --checkpoint /nfs/home/nglazman/results/vqvae/checkpoint_best.pt \
+    --save rf_combined.png
